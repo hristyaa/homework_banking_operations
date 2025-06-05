2d360e12dd20ea33d7dca0345c6688f656f9e1ac
Hristina Nadelyaeva <shhs200799@gmail.com>
2025-06-05 20:35:41 +0900

    get_mask_account and get_mask_card_number

---
+++ src/masks.py
@@ -0,0 +1,21 @@
+def get_mask_card_number(card_number: int) -> str:
+    """Функция, которая возвращает маску номера карты  по правилу XXXX XX** **** XXXX"""
+    if len(str(card_number)) == 16:  # проверка номера карта на правильность по количеству символов
+        mask_card_number = str(card_number)[:4] + " " + str(card_number)[4:6] + "** **** " + str(card_number)[-4:]
+        return mask_card_number
+    else:
+        return "Введенного номера карты не существует"
+
+
+def get_mask_account(account_number: int) -> str:
+    """Функция, которая возвращает маску номера счета пользователя по правилу **XXXX"""
+    if len(str(account_number)) == 20:  # проверка номера счета на правильность по количеству символов
+        return "**" + str(account_number)[-4:]
+    else:
+        return "Введенного номера счета не существует"
+
+
+user_card_number = int(input("Введите номер карты: "))
+print(get_mask_card_number(user_card_number))
+user_account_number = int(input("Введите номер cчета: "))
+print(get_mask_account(user_account_number))
