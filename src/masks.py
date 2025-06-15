8162221aca4152fb59431ecafc9831493f7812a3
Hristina Nadelyaeva <shhs200799@gmail.com>
2025-06-15 16:56:15 +0900

    sending to the GitHub

--- src/masks.py
+++ src/masks.py
@@ -14,7 +14,8 @@ def get_mask_account(account_number: int) -> str:
     else:
         return "Введенного номера счета не существует"
 
-#user_card_number = int(input("Введите номер карты: "))
+
+# user_card_number = int(input("Введите номер карты: "))
 get_mask_card_number(7000792289606361)
-#user_account_number = int(input("Введите номер cчета: "))
+# user_account_number = int(input("Введите номер cчета: "))
 get_mask_account(73654108430135874305)
