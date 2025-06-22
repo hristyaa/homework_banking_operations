1fabe74617e963677084141deca4a323d43cc0b6
Hristina Nadelyaeva <shhs200799@gmail.com>
2025-06-22 18:35:16 +0900

    test_get_mask_card_number + str

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.masks import get_mask_card_number
+from src.masks import get_mask_card_number, get_mask_account
 
 
 def test_get_mask_card_number_1():
@@ -9,9 +9,10 @@ def test_get_mask_card_number_1():
 
 
 @pytest.mark.parametrize('card_number, expected', [(7000792289606361, '7000 79** **** 6361'),
-                                                   (7000792289604785, '7000 79** **** 4785')])
+                                                (7000792289604785, '7000 79** **** 4785'),
+                                                ('7000792289606361', '7000 79** **** 6361')])
 def test_get_mask_card_number_2(card_number, expected):
-    """Проверка работы функции на различных входных данных номеров карт"""
+    """Проверка работы функции на различных форматах входных данных номеров карт"""
     assert get_mask_card_number(card_number) == expected
 
 
@@ -21,8 +22,11 @@ def test_get_mask_card_number_invalid_card_number_0():
         get_mask_card_number('')
 
 
-def test_get_mask_card_number_invalid_card_number():
+def test_get_mask_card_number_invalid_card_number_lenght():
     """Проверка, что функция корректно обрабатывает входные строки, где длина номера карты неправильная"""
     with pytest.raises(ValueError):
         get_mask_card_number(7000792289604785123)
+
+
+
 
