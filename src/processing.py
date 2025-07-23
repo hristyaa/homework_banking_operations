7efc9c9a789ef4fd80dac4d1a8461b0b9b67ead1
Hristyaa <shhs200799@gmail.com>
2025-07-23 22:22:40 +0900

    process_bank_search

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,5 @@
+import re
+
 def filter_by_state(banking_transaction_data: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Функция фильтрует список банковских операций по статусу."""
     new_list_data = []
@@ -15,24 +17,48 @@ def sort_by_date(banking_transaction_data: list[dict], reverse: bool = True) -> 
     return sorted_banking_transaction_data
 
 
-print(
-    filter_by_state(
-        [
-            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-        ]
-    )
-)
+def process_bank_search(banking_transaction_data:list[dict], search:str)->list[dict]:
+    """ Функция возвращает список словарей транзакций, в которых в описании есть данная строка"""
+    try:
+        pattern = re.compile(search, re.IGNORECASE)
+        new_data = []
+        for transaction in banking_transaction_data:
+            if pattern.search(transaction.get('description')) is not None:
+                new_data.append(transaction)
+        return new_data
+    except Exception as ex:
+        return f'Возкникла ошибка:"{ex}"'
 
-print(
-    sort_by_date(
-        [
-            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-        ]
-    )
-)
+
+# print(
+#     process_bank_search(
+#         [
+#             {"id": 41428829, "description": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+#             {"id": 939719570, "description": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+#             {"id": 594226727, "description": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+#             {"id": 615064591, "description": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+#         ], 'executed'
+#     )
+# )
+
+# print(
+#     filter_by_state(
+#         [
+#             {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+#             {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+#             {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+#             {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+#         ]
+#     )
+# )
+#
+# print(
+#     sort_by_date(
+#         [
+#             {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+#             {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+#             {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+#             {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+#         ]
+#     )
+# )
